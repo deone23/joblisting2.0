5426ed24af03800b2a9b8fddd9f6aab20340d065
deone23 <vinaymourya1234@gmail.com>
2025-03-21 19:57:25 +0530

    first commit

---
+++ joblisting/src/main/java/com/telusko/joblisting/repository/SearchRepository.java
@@ -0,0 +1,10 @@
+package com.telusko.joblisting.repository;
+
+import java.util.List;
+
+import com.telusko.joblisting.model.Post;
+
+public interface SearchRepository {
+
+  	List<Post> findByText(String text);
+}
